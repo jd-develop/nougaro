64fa8d41511fba7ad13c04de4187179b89b10ca6
Jean Dubois <jd-dev@laposte.net>
2022-05-26 21:11:43 +0200

    update build.bat
    now it works on every PC with Python

--- build.bat
+++ build.bat
@@ -1,21 +1,24 @@
 @echo off
 pause
+
+rem We update pip and nuitka
 python -m pip install --upgrade pip nuitka
-del .\.gitignore
-del .\.gitattributes
-del example_file
-del grammar.txt
-del tests.noug
-del todo.md
-rmdir /s /q .\.git
-rmdir /s /q .\.github
-rmdir /s /q .\.vscode
-rmdir /s /q .\.idea
-rmdir /s /q .\__pycache__
-rmdir /s /q src\__pycache__
-rmdir /s /q src\values\__pycache__
-rmdir /s /q src\values\functions\__pycache__
-rmdir /s /q src\values\specific_values\__pycache__
-rmdir /s /q lib_\__pycache__
+
+rem We delete useless files and directories
+for %%y in (.\.gitignore, .\.gitattributes, example_file, grammar.txt, tests.noug, todo.md) do if exist %%y (del %%y)
+
+for %%y in (.\.git, .\.github, .\.vscode, .\.idea, .\__pycache__, src\__pycache__, src\values\__pycache__, src\values\functions\__pycache__, src\values\specific_values\__pycache__, lib_\__pycache__, sandbox) do if exist %%y (rmdir /s /q %%y)
+
+rem Then we ask for the version
 set /p NOUGVERSION="Nougaro version: "
+
+rem We build
 python -m nuitka --standalone --windows-company-name=Nougaro --windows-product-name=Nougaro --windows-product-version=%NOUGVERSION% shell.py
+
+rem We copy the important files to the created directory
+for %%y in (example.noug "highlight theme for NPP.xml" LICENSE noug_version.json README.md shell.py) do xcopy %%y shell.dist\
+
+for %%y in (examples lib_ src) do xcopy /s /i %%y shell.dist\%%y
+
+rem Then we rename our directory
+RENAME shell.dist nougaro-"%NOUGVERSION%"-alpha-windows-exe

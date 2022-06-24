de5b7066cde5260a32b164fb3ea459fe910553b0
Jean Dubois <jd-dev@laposte.net>
2022-06-24 17:18:32 +0200

    (1/2) update to match with GPL recommendations

--- build.bat
+++ build.bat
@@ -1,4 +1,20 @@
 @echo off
+rem Nougaro : a python-interpreted high-level programming language
+rem Copyright (C) 2021-2022  Jean Dubois (https://github.com/jd-develop) jd-dev@laposte.net
+rem
+rem This program is free software: you can redistribute it and/or modify
+rem it under the terms of the GNU General Public License as published by
+rem the Free Software Foundation, either version 3 of the License, or
+rem (at your option) any later version.
+rem
+rem This program is distributed in the hope that it will be useful,
+rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem GNU General Public License for more details.
+rem
+rem You should have received a copy of the GNU General Public License
+rem along with this program.  If not, see https://www.gnu.org/licenses/.
+
 pause
 
 rem We update pip and nuitka
--- src/nougaro.py
+++ src/nougaro.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-# this file is part of the NOUGARO language, created by Jean Dubois (https://github.com/jd-develop)
-# Public Domain
+
+# Nougaro : a python-interpreted high-level programming language
+# Copyright (C) 2021-2022  Jean Dubois (https://github.com/jd-develop) <jd-dev@laposte.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # Actually running with Python 3.9, works with Python 3.10
 
 # IMPORTS

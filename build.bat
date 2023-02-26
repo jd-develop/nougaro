049ae8780d426037acda3321d64bc353a124f172
Jean Dubois <jd-dev@laposte.net>
2023-02-26 19:52:58 +0100

    delete useless GPL comments

--- build.bat
+++ build.bat
@@ -1,17 +1,7 @@
 @echo off
 rem Nougaro : a python-interpreted high-level programming language
 rem Copyright (C) 2021-2023  Jean Dubois (https://github.com/jd-develop) jd-dev@laposte.net
-rem
-rem This program is free software: you can redistribute it and/or modify
-rem it under the terms of the GNU General Public License as published by
-rem the Free Software Foundation, either version 3 of the License, or
-rem (at your option) any later version.
-rem
-rem This program is distributed in the hope that it will be useful,
-rem but WITHOUT ANY WARRANTY; without even the implied warranty of
-rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-rem GNU General Public License for more details.
-rem
+
 rem You should have received a copy of the GNU General Public License
 rem along with this program.  If not, see https://www.gnu.org/licenses/.
 

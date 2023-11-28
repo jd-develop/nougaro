77a14b37639543aeee59e954374288fe93a655d9
Jean Dubois <jd-dev@laposte.net>
2023-11-28 15:15:39 +0100

    fix tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-python3 shell.py -c "__test__()"
+python3 shell.py tests/test_file.noug
 # python3 shell.py -c "__test__(1)"  # use this to print "OK (thing)" when (thing) is tested, useful for debugging
 return_code=$?
 if [ $return_code != 0 ]; then exit $return_code; fi

a6746b6e1d3df80359a6a5bc2bcd5150d3c9c559
Jean Dubois <jd-dev@laposte.net>
2023-11-28 15:19:54 +0100

    fix tests (3)

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+echo 0 > config/SHOULD_TEST_PRINT_OK
 python3 shell.py tests/test_file.noug
 # python3 shell.py -c "__test__(1)"  # use this to print "OK (thing)" when (thing) is tested, useful for debugging
 return_code=$?
